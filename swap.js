895f2dee1d70658e2e05cfdfa49c7f1d685c230f
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-02-11 23:12:34 +0600

    Added swap.js file

---
+++ swap.js
@@ -0,0 +1,13 @@
+var first = 7;
+var second = 9;
+console.log(first, second);
+
+var temp = first;
+first = second;
+second = temp;
+
+console.log(first, second);
+
+
+[first, second] = [second, first]
+console.log(first, second);
