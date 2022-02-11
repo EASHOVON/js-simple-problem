7d76ee40e062f0d99d51d9560a6f116798fe1426
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-02-11 21:10:54 +0600

    first commit

---
+++ math.js
@@ -0,0 +1,15 @@
+// Math.abs, Math.ceil, Math.floor, Math.round, Math.random
+
+const negetiveNumber = -5;
+const convertToPositiveNumber = Math.abs(negetiveNumber);
+// console.log(convertToPositiveNumber);
+
+const numberWithFraction = 5.567;
+const numberUp = Math.ceil(numberWithFraction);
+// console.log(numberUp);
+const numberDown = Math.floor(numberWithFraction);
+// console.log(numberDown);
+const roundNumber = Math.round(numberWithFraction);
+// console.log(roundNumber);
+const randomNumber = Math.random() * 6;
+console.log(Math.ceil(randomNumber));
