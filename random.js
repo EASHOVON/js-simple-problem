43c5721c8b8e39801967de562912c70702952fa1
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-02-13 22:02:34 +0600

    Tried some Math function

---
+++ random.js
@@ -0,0 +1,16 @@
+const selected = [];
+for (let i = 0; i < 10; i++)
+{
+    const random = Math.random() * 100;
+    const picked = Math.round(random);
+    if (selected.indexOf(picked) == -1)
+    {
+        selected.push(picked)
+    }
+    else
+    {
+        console.log(selected, picked);
+    }
+}
+
+console.log(selected);
